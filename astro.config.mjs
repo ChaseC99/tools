2794053ca8c56a8e01501fde0c5d3683fc9bdc83
Chase Carnaroli <chasecarnaroli@gmail.com>
2024-09-21 01:34:48 -0700

    Add svelte, react, and vue to the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
+import vue from "@astrojs/vue";
+import react from "@astrojs/react";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://tools.chase.link',
+  site: 'https://tools.chase.link',
+  integrations: [vue(), react(), svelte()]
 });
